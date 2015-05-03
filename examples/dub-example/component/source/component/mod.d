312ea390d5fb726a1c876c5c898b4bcc1cd64634
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2015-05-03 14:33:07 +0300

    Updated comment in dub-example/component/source/component/mod.d

--- examples/dub-example/component/source/component/mod.d
+++ examples/dub-example/component/source/component/mod.d
@@ -32,7 +32,7 @@ unittest
 {
 	auto app = appender!string;
 
-	//A string that contains C or D code can be framed with `q{` and `}`
+	//A string that contains a code with C-like block syntax can be framed with `q{` and `}`
 	immutable text0 = q{
 // main function
 int main()
